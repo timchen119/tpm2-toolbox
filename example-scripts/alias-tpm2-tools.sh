6f9c6bf8308a1d9ea2d10185b900d8198c45acfd
Tim Chen <tim.chen119@canonical.com>
2019-11-12 11:31:41 +0800

    Add tpm2_policytemplate and an example script (alias-tpm2-tools.sh) for alias

---
+++ example-scripts/alias-tpm2-tools.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+for binary in /snap/tpm2-toolbox/current/bin/tpm2_*; do command=$(basename $binary | cut -c 6-); sudo snap alias tpm2-toolbox.$(echo $command | sed 's/_/-/g') tpm2_$command; done

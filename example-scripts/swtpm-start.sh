d3d57348f8453e133a026e77b356d316e3873ece
Jian-Ding Chen (timchen119) <tim.chen119@canonical.com>
2020-12-29 13:41:29 +0800

    Fix binaries location in example scripts

--- example-scripts/swtpm-start.sh
+++ example-scripts/swtpm-start.sh
@@ -16,4 +16,4 @@ cleanup() {
 trap cleanup INT TERM EXIT
 
 echo "start swtpm with TPM state path $TPMSTATE_PATH and socket path $SOCK_PATH"
-sh -c "$SNAP/bin/swtpm $* socket --tpmstate dir=$TPMSTATE_PATH --tpm2 --ctrl type=unixio,path=$SOCK_PATH --log file=$LOG_PATH,level=9"
+sh -c "$SNAP/usr/local/bin/swtpm $* socket --tpmstate dir=$TPMSTATE_PATH --tpm2 --ctrl type=unixio,path=$SOCK_PATH --log file=$LOG_PATH,level=9"
